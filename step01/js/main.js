9f8a146da5647ce6f824fa36ca7c3a7447b9db6f
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-18 22:43:17 +0900

    グラフデータの2つめまでを横棒グラフにセット

--- step01/js/main.js
+++ step01/js/main.js
@@ -2,9 +2,18 @@
 var dataSet = [300, 130, 5, 60, 240];
 
 // rectタグの生成
+// データ1つめ
 d3.select('#myGraph')
   .append('rect')
   .attr('x', 0)
   .attr('y', 0)
   .attr('width', dataSet[0])
+  .attr('height', '20px')
+
+// データ2つめ
+d3.select('#myGraph')
+  .append('rect')
+  .attr('x', 0)
+  .attr('y', 25)
+  .attr('width', dataSet[1])
   .attr('height', '20px')
