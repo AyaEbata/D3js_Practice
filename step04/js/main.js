e188974bfc4a7d65b91581a6951739a1f89e844e
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-19 20:08:18 +0900

    csvファイルの日本語表記のヘッダーを英語表記に変換

--- step04/js/main.js
+++ step04/js/main.js
@@ -1,22 +1,33 @@
-d3.csv('data/data.csv', function(error, data) {
-    var dataSet = [];
-    for (var i = 0; i < data.length; i++) {
-        // data.csvのヘッダー部分が日本語の場合、このように指定する
-        dataSet.push(data[i]['商品１']);
-    }
+d3.csv('data/data.csv')
+  .row(function(d) {
+      return {
+          // 日本語表記を英語に変換（てゆか、d['商品１']をitem1に入れた的な）
+          item1: d['商品１'],
+          item2: d['商品２'],
+          item3: d['商品３']
+      };
+  })
+  .get(function(error, data) {  // .get()を使うと、csvファイルが読み込み終わった後に実行される
+      console.log(data);
 
-    // グラフの描画
-    d3.select('#myGraph')
-      .selectAll('rect')
-      .data(dataSet)
-      .enter()
-      .append('rect')
-      .attr('x', 0)
-      .attr('y', function(d, i) {
-          return i * 25;
-      })
-      .attr('width', function(d, i) {
-          return d + 'px';
-      })
-      .attr('height', '20px')
-})
+      var dataSet = [];
+      for (var i = 0; i < data.length; i++) {
+          // 日本語表記から英語表記に変換したからitem1でいける
+          dataSet.push(data[i].item1);
+      }
+
+      // グラフの描画
+      d3.select('#myGraph')
+        .selectAll('rect')
+        .data(dataSet)
+        .enter()
+        .append('rect')
+        .attr('x', 0)
+        .attr('y', function(d, i) {
+            return i * 25;
+        })
+        .attr('width', function(d, i) {
+            return d + 'px';
+        })
+        .attr('height', '20px')
+  })
