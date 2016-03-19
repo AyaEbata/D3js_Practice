7e36b564d8791dd3d8d084319b8563be2b49f090
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-19 23:11:33 +0900

    call()とeach()を使うと、datum()のようなことが可能

--- step05/js/main.js
+++ step05/js/main.js
@@ -5,7 +5,13 @@ d3.select('#myGraph')
   .data(dataSet)
   .enter()
   .append('rect')
-  .datum(function(d, i) {  // .datum()でグラフデータの値を変更できる
-      console.log('d = ' + d + ', i = ' + i);
-      return d;
+  // .datum(function(d, i) {  // .datum()でグラフデータの値を変更できる
+  //     console.log('d = ' + d + ', i = ' + i);
+  //     return d;
+  // })
+  .call(function(elem) {  // call()とeach()で上記datum()と同じ出力結果は得られる
+      elem.each(function(d, i) {
+          console.log('d = ' + d + ', i = ' + i);
+      });
   })
+  
