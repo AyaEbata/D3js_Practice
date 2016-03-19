aabc98faf6b8a753d124808050cafd27f768cd5a
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-19 14:41:16 +0900

    CSVファイルからデータを読み込むように変更

--- step01/js/main.js
+++ step01/js/main.js
@@ -1,49 +1,60 @@
-// グラフのデータ
-var dataSet = [300, 130, 5, 60, 240];
+// CSVファイルからデータの読み込み
+d3.csv('data/data.csv', function(error, data) {
+    var dataSet = [];
 
-// rectタグの生成
-d3.select('#myGraph')
-  .selectAll('rect')
-  .data(dataSet)
-  .enter()  // 自動的にdataSetの数だけrectが生成される
-  .append('rect')
-  .attr('x', 0)
-  .attr('y', function(d, i) {  // (0, 25*i)が各rectのスタート位置
-      return i * 25;
-  })
-  .attr('width', '0px')    // transition()でアニメーションをつけるにあたって、一旦0pxにしておく
-  .attr('height', '20px')
-  .transition()            // グラフのアニメーション
-  .delay(function(d, i) {  // 1つずつ順番に時間ずらしてアニメーション
-      return i * 300;      // 0.3秒ごと
-  })
-  .duration(1500)          // 1.5秒かけてアニメーションを実施
-  .attr('width', function(d, i) {  // dにdataSetの値が入っているので、順番に出力
-      return d + 'px';
-  })
+    // item1のデータをセット
+    for (var i = 0; i < data.length; i++) {
+        dataSet.push(data[i].item1);
+    }
 
-// グラフがクリックされたとき
-d3.select('#myGraph')
-  .selectAll('rect')
-  .on('click', function() {
-      d3.select(this)  // thisでクリックしたグラフの棒のみを指定できる
-        .style('fill', '#F8BBD0')
-  })
+    // rectタグの生成
+    d3.select('#myGraph')
+      .selectAll('rect')
+      .data(dataSet)
+      .enter()  // 自動的にdataSetの数だけrectが生成される
+      .append('rect')
+      .attr('x', 0)
+      .attr('y', function(d, i) {  // (0, 25*i)が各rectのスタート位置
+          return i * 25;
+      })
+      .attr('width', '0px')    // transition()でアニメーションをつけるにあたって、一旦0pxにしておく
+      .attr('height', '20px')
+      .transition()            // グラフのアニメーション
+      .delay(function(d, i) {  // 1つずつ順番に時間ずらしてアニメーション
+          return i * 300;      // 0.3秒ごと
+      })
+      .duration(1500)          // 1.5秒かけてアニメーションを実施
+      .attr('width', function(d, i) {  // dにdataSetの値が入っているので、順番に出力
+          return d + 'px';
+      })
 
-// ボタンがクリックされたときグラフのデータを更新
-d3.select('#updateButton')
-  .on('click', function() {
+    // グラフがクリックされたとき
+    d3.select('#myGraph')
+      .selectAll('rect')
+      .on('click', function() {
+          d3.select(this)  // thisでクリックしたグラフの棒のみを指定できる
+            .style('fill', '#F8BBD0')
+      })
 
-      // 更新するグラフのデータ
-      dataSet = [20, 230, 150, 10, 20];
+    // ボタンがクリックされたときグラフのデータを更新
+    d3.select('#updateButton')
+      .on('click', function() {
 
-      // 更新するグラフ
-      d3.select('#myGraph')
-        .selectAll('rect')
-        .data(dataSet)
-        .transition()    // グラフのアニメーション
-        .duration(1500)  // 1.5秒かけてアニメーションを実施
-        .attr('width', function(d, i) {
-            return d + 'px';
-        })
-  })
+          dataSet = [];
+
+          // item2のデータをセット
+          for (var i = 0; i < data.length; i++) {
+              dataSet.push(data[i].item2);
+          }
+
+          // 更新するグラフ
+          d3.select('#myGraph')
+            .selectAll('rect')
+            .data(dataSet)
+            .transition()    // グラフのアニメーション
+            .duration(1500)  // 1.5秒かけてアニメーションを実施
+            .attr('width', function(d, i) {
+                return d + 'px';
+            })
+      })
+})
