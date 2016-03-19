f211d57f663acb9e2ce35ac8c54f63a3b95be2d6
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-19 18:53:37 +0900

    属性(attr)をまとめて書くことも可能

--- step03/js/main.js
+++ step03/js/main.js
@@ -1,8 +1,9 @@
 d3.select('#myGraph')
   .append('svg')
   .append('rect')
-  .attr('x', '10px')
-  .attr('y', '20px')
-  .attr('width', '100px')
-  .attr('height', '80px')
-
+  .attr({
+      x: '10px',
+      y: '20px',
+      width: '100px',
+      height: '80px'
+  })
