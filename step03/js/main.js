1b9441b53cc173a9984e6dfab8f5ae0bb5481761
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-19 19:27:49 +0900

    色とか指定する際は、cssで先に指定しておき、そのclassをD3.jsで指定してあげる方が良い

--- step03/js/main.js
+++ step03/js/main.js
@@ -5,6 +5,12 @@ d3.select('#myGraph')
       x: '10px',
       y: '20px',
       width: '100px',
-      height: '80px'
+      height: '80px',
+      id: 'rectangle'
   })
-  .style('fill', '#F50057')
+
+d3.select('#rectangle')
+  .on('click', function() {
+      d3.select(this)
+        .attr('class', 'bar_color')  // cssで先に作成していたclassを指定してあげるだけ
+  })
