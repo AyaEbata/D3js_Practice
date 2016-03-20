903b394a9cb181136970972c04d5694961fcde62
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-20 21:24:35 +0900

    csvファイルから読み込むように変更

--- step07/js/main.js
+++ step07/js/main.js
@@ -1,5 +1,4 @@
-// 使用するデータ
-var dataSet = [45, 25, 15, 10, 5];
+// 色の指定
 var colorList = ['#FF4081', '#536DFE', '#E040FB', '#8BC34A', '#FFC107'];
 
 // 円グラフレイアウト
@@ -10,55 +9,63 @@ var arc = d3.svg.arc()
   .innerRadius(40)
   .outerRadius(100)
 
-// グラフの描画
-var pieElem = d3.select('#myGraph')
-  .selectAll('g')
-  .data(pie(dataSet))
-  .enter()
-  .append('g')
-  .attr('transform', 'translate(110, 105)')
+// csvファイルの読み込み
+d3.csv('data/data1.csv', function(error, data) {
+    var dataSet = [];
+    for (var i in data[0]) {
+        dataSet.push(data[0][i]);
+    }
 
-// データの追加
-pieElem
-  .append('path')
-  .attr('class', 'pie')
-  // .attr('d', arc)  // アニメーションをつける時は、ここで指定しなくなる
-  .style('fill', function(d, i) {
-      return colorList[i];
-  })
+    // グラフの描画
+    var pieElem = d3.select('#myGraph')
+      .selectAll('g')
+      .data(pie(dataSet))
+      .enter()
+      .append('g')
+      .attr('transform', 'translate(110, 105)')
 
-  // アニメーション
-  .transition()
-  .duration(700)
-  .delay(function(d, i) {
-      return i * 640;
-  })
-  .ease('sin-out')  // アニメーションの波形を変更
-  .attrTween('d', function(d, i) {
-      var interpolate = d3.interpolate(
-          { startAngle: d.startAngle, endAngle: d.startAngle },  // アニメーション始まる前の位置
-          { startAngle: d.startAngle, endAngle: d.endAngle }     // アニメーション終わってからの位置
-      )
-      return function(t) {  // t = 時間
-          return arc(interpolate(t));
-      }
-  })
+    // データの追加
+    pieElem
+      .append('path')
+      .attr('class', 'pie')
+      // .attr('d', arc)  // アニメーションをつける時は、ここで指定しなくなる
+      .style('fill', function(d, i) {
+          return colorList[i];
+      })
 
-// 各データの値を円弧内に表示
-pieElem
-  .append('text')
-  .attr('class', 'pie-val')
-  .attr('transform', function(d, i) {
-      return 'translate(' + arc.centroid(d) + ')';
-  })
-  .text(function(d, i) {
-      console.log(d);  // (中身みてみ)
-      return d.value;
-  })
+      // アニメーション
+      .transition()
+      .duration(700)
+      .delay(function(d, i) {
+          return i * 640;
+      })
+      .ease('sin-out')  // アニメーションの波形を変更
+      .attrTween('d', function(d, i) {
+          var interpolate = d3.interpolate(
+              { startAngle: d.startAngle, endAngle: d.startAngle },  // アニメーション始まる前の位置
+              { startAngle: d.startAngle, endAngle: d.endAngle }     // アニメーション終わってからの位置
+          )
+          return function(t) {  // t = 時間
+              return arc(interpolate(t));
+          }
+      })
 
-// テキストの表示(中央に合計値)
-var textElem = d3.select('#myGraph')
-  .append('text')
-  .attr('class', 'sum-label')
-  .attr('transform', 'translate(110, ' + (105 + 5) + ')')
-  .text('SUM ' + d3.sum(dataSet))
+    // 各データの値を円弧内に表示
+    pieElem
+      .append('text')
+      .attr('class', 'pie-val')
+      .attr('transform', function(d, i) {
+          return 'translate(' + arc.centroid(d) + ')';
+      })
+      .text(function(d, i) {
+          console.log(d);  // (中身みてみ)
+          return d.value;
+      })
+
+    // テキストの表示(中央に合計値)
+    var textElem = d3.select('#myGraph')
+      .append('text')
+      .attr('class', 'sum-label')
+      .attr('transform', 'translate(110, ' + (105 + 5) + ')')
+      .text('SUM ' + d3.sum(dataSet))
+})
