03c92b21d18be70b8ff2ee58cf55e2cf477fe842
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-20 13:01:50 +0900

    グラフ内に数値を表示

--- step06/js/main.js
+++ step06/js/main.js
@@ -9,6 +9,7 @@ var barElem = d3.select('#myGraph')
   .selectAll('rect')
   .data(dataSet)
 
+// データの追加
 barElem
   .enter()
   .append('rect')
@@ -23,4 +24,18 @@ barElem
   .attr('y', function(d, i) {
       // ここで取得したheightを使う(使わないと逆さまになる)
       return svgHeight - d;
+  })
+
+// テキストの追加
+barElem
+  .enter()
+  .append('text')
+  .attr('class', 'bar-label')
+  .attr('x', function(d, i) {
+      // グラフのwidthが20だから、真ん中を選択するために10を足す
+      return i * 25 + 10;
+  })
+  .attr('y', svgHeight - 5)
+  .text(function(d, i) {
+      return d;
   })
