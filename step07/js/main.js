7667224251589ea91c26785d751cb5e14c54e867
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-20 21:43:13 +0900

    csvファイルをselectで変更できるように変更

--- step07/js/main.js
+++ step07/js/main.js
@@ -9,63 +9,94 @@ var arc = d3.svg.arc()
   .innerRadius(40)
   .outerRadius(100)
 
-// csvファイルの読み込み
-d3.csv('data/data1.csv', function(error, data) {
-    var dataSet = [];
-    for (var i in data[0]) {
-        dataSet.push(data[0][i]);
-    }
 
-    // グラフの描画
-    var pieElem = d3.select('#myGraph')
-      .selectAll('g')
-      .data(pie(dataSet))
-      .enter()
-      .append('g')
-      .attr('transform', 'translate(110, 105)')
+// 初期表示
+drowPie('data1');
 
-    // データの追加
-    pieElem
-      .append('path')
-      .attr('class', 'pie')
-      // .attr('d', arc)  // アニメーションをつける時は、ここで指定しなくなる
-      .style('fill', function(d, i) {
-          return colorList[i];
-      })
+// selectが変更された時
+d3.select('#select-data')
+  .on('change', function() {
 
-      // アニメーション
-      .transition()
-      .duration(700)
-      .delay(function(d, i) {
-          return i * 640;
-      })
-      .ease('sin-out')  // アニメーションの波形を変更
-      .attrTween('d', function(d, i) {
-          var interpolate = d3.interpolate(
-              { startAngle: d.startAngle, endAngle: d.startAngle },  // アニメーション始まる前の位置
-              { startAngle: d.startAngle, endAngle: d.endAngle }     // アニメーション終わってからの位置
-          )
-          return function(t) {  // t = 時間
-              return arc(interpolate(t));
-          }
-      })
+      // 変更されたらいったん表示してるの消す
+      d3.select('#myGraph')
+        .selectAll('*')
+        .remove()
 
-    // 各データの値を円弧内に表示
-    pieElem
-      .append('text')
-      .attr('class', 'pie-val')
-      .attr('transform', function(d, i) {
-          return 'translate(' + arc.centroid(d) + ')';
-      })
-      .text(function(d, i) {
-          console.log(d);  // (中身みてみ)
-          return d.value;
-      })
+      drowPie(this.value);
+  })
 
-    // テキストの表示(中央に合計値)
-    var textElem = d3.select('#myGraph')
-      .append('text')
-      .attr('class', 'sum-label')
-      .attr('transform', 'translate(110, ' + (105 + 5) + ')')
-      .text('SUM ' + d3.sum(dataSet))
-})
+
+// 円グラフの描画メソッド
+function drowPie(fileName) {
+
+    // csvファイルの読み込み
+    d3.csv('data/' + fileName + '.csv', function(error, data) {
+        var dataSet = [];
+        for (var i in data[0]) {
+            dataSet.push(data[0][i]);
+        }
+
+        // dataSetを降順に変換(そうじゃないとアニメーション崩れる)
+        dataSet.sort(function(a, b) {
+            a = parseInt(a);
+            b = parseInt(b);
+
+            if( a > b ) return -1;
+            if( a < b ) return 1;
+            return 0;
+        });
+
+        // グラフの描画
+        var pieElem = d3.select('#myGraph')
+          .selectAll('g')
+          .data(pie(dataSet))
+          .enter()
+          .append('g')
+          .attr('transform', 'translate(110, 105)')
+
+        // データの追加
+        pieElem
+          .append('path')
+          .attr('class', 'pie')
+          // .attr('d', arc)  // アニメーションをつける時は、ここで指定しなくなる
+          .style('fill', function(d, i) {
+              return colorList[i];
+          })
+
+          // アニメーション
+          .transition()
+          .duration(700)
+          .delay(function(d, i) {
+              return i * 640;
+          })
+          .ease('sin-out')  // アニメーションの波形を変更
+          .attrTween('d', function(d, i) {
+              var interpolate = d3.interpolate(
+                  { startAngle: d.startAngle, endAngle: d.startAngle },  // アニメーション始まる前の位置
+                  { startAngle: d.startAngle, endAngle: d.endAngle }     // アニメーション終わってからの位置
+              )
+              return function(t) {  // t = 時間
+                  return arc(interpolate(t));
+              }
+          })
+
+        // 各データの値を円弧内に表示
+        pieElem
+          .append('text')
+          .attr('class', 'pie-val')
+          .attr('transform', function(d, i) {
+              return 'translate(' + arc.centroid(d) + ')';
+          })
+          .text(function(d, i) {
+              console.log(d);  // (中身みてみ)
+              return d.value;
+          })
+
+        // テキストの表示(中央に合計値)
+        var textElem = d3.select('#myGraph')
+          .append('text')
+          .attr('class', 'sum-label')
+          .attr('transform', 'translate(110, ' + (105 + 5) + ')')
+          .text('SUM ' + d3.sum(dataSet))
+    })
+}
