23582d8d2a988f7d7f81fa9f285d469dfb469e5d
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-20 10:02:20 +0900

    データの更新(更新だからグラフ数は固定)をなんの変哲もなくそれなりに書くとこうなる

--- step05/js/main.js
+++ step05/js/main.js
@@ -1,17 +1,51 @@
-var dataSet = [73, 128, 49, 95, 63];
+// 初期表示
+var csvFile = document.getElementById('test1').getAttribute('data-src');
 
-d3.select('#myGraph')
-  .selectAll('rect')
-  .data(dataSet)
-  .enter()
-  .append('rect')
-  // .datum(function(d, i) {  // .datum()でグラフデータの値を変更できる
-  //     console.log('d = ' + d + ', i = ' + i);
-  //     return d;
-  // })
-  .call(function(elem) {  // call()とeach()で上記datum()と同じ出力結果は得られる
-      elem.each(function(d, i) {
-          console.log('d = ' + d + ', i = ' + i);
-      });
+d3.csv('data/' + csvFile, function(error, data) {
+
+    // dataSetにdata1.csvのデータを入れる
+    var dataSet = [];
+    for (var i = 0; i < data.length; i++) {
+        dataSet.push(data[i]['商品１']);
+    }
+
+    // 描画
+    d3.select('#myGraph')
+      .selectAll('rect')
+      .data(dataSet)
+      .enter()
+      .append('rect')
+      .attr('x', 0)
+      .attr('y', function(d, i) {
+          return i * 25;
+      })
+      .attr('width', function(d, i) {
+          return d + 'px';
+      })
+      .attr('height', '20px')
+})
+
+// csvファイル名が書かれたボタンが押されたとき
+d3.selectAll('button')
+  .on('click', function() {
+
+      // 押されたファイル名を取得
+      csvFile = this.getAttribute('data-src');
+      d3.csv('data/' + csvFile, function(error, data) {
+
+          // 押されたファイルのデータをdataSetに入れる
+          var dataSet = [];
+          for (var i = 0; i < data.length; i++) {
+              dataSet.push(data[i]['商品１']);
+          }
+
+          // グラフの棒の長さのみ更新（だから、グラフの棒の数は初期表示と一緒）
+          d3.select('#myGraph')
+            .selectAll('rect')
+            .data(dataSet)
+            .attr('width', function(d, i) {
+                return d + 'px';
+            })
+      })
   })
   
